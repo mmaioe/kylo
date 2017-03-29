d5c268073d29b0d8a66f4924ce49b4a20769d3d4
scottreisdorf <scott.reisdorf@thinkbiganalytics.com>
2017-03-28 21:54:50 -0700

    Entity Level Access Control Role Assignment Dialog

--- ui/ui-app/src/main/resources/static/js/feed-mgr/module-require.js
+++ ui/ui-app/src/main/resources/static/js/feed-mgr/module-require.js
@@ -28,7 +28,9 @@ define(['angular',
         'feed-mgr/shared/nifi-property-input/nifi-property-timunit-input',
         'feed-mgr/shared/nifi-property-input/nifi-property-input',
         'feed-mgr/shared/cron-expression-validator/cron-expression-validator',
-        'feed-mgr/shared/cron-expression-preview/cron-expression-preview'],function() {
+        'feed-mgr/shared/cron-expression-preview/cron-expression-preview',
+        'feed-mgr/shared/entity-access-control/entity-access',
+        'feed-mgr/shared/entity-access-control/EntityAccessControlService'],function() {
 
 });
 
